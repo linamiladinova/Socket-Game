18676c828d1e046655dfb1f9af46d7009ab5e270
Lina Miladinova <46829333+linamiladinova@users.noreply.github.com>
2021-06-21 10:05:30 +0300

    Add files via upload

---
+++ client/ui.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import threading
+
+HOST = "127.0.0.1"
+PORT = 6666
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+w, h = 60, 30
+
+global free_row
+free_row = 1
+
+grid = [["#" for x in range(w)] for y in range(h)]
+
+def render():
+    os.system("clear")
+    for y in range(h):
+        for x in range(w):
+            print(grid[y][x], end ="")
+        print("")
+
+for y in range(int(h-2)):
+    for x in range(int(w-2)):
+        grid[y+1][x+1] = " "
+
+render()
+
+def receive():
+    global free_row
+    while(1):
+        answer = (s.recv(1024).decode("utf-8"))
+        print(answer)
+        if answer != " ":
+            for i in range(len(answer)):
+                grid[free_row][i+2] = answer[i]
+            render()
+            free_row += 1
+
+x = threading.Thread(target = receive)
+
+x.start()
+
+while (free_row != 20):
+    string = input(" > ")
+    s.send(string.encode("utf-8"))
+
+s.close()
---
+++ server/server.py
@@ -0,0 +1,45 @@
+import socket
+import os
+from _thread import *
+import threading
+
+ServerSocket = socket.socket()
+host = '127.0.0.1'
+port = 6666
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+
+global clients
+clients = set()
+global clients_lock
+clients_lock = threading.Lock()
+
+
+def threaded_client(connection):
+    global clients
+    global clients_lock
+    clients.add(connection)
+    connection.send(str.encode('Welcome to ShitChat!'))
+    while True:
+        data = connection.recv(1024)
+        reply = data.decode('utf-8')
+        if not data:
+            break
+        with clients_lock:
+            for c in clients:
+                c.sendall(str.encode(reply))
+        connection.sendall(str.encode(reply))
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSocket.close()
